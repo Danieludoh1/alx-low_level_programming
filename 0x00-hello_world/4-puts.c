cd53680bb3e6541d142cf5b5ba67fa73f0959c12
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-16 13:45:35 -0800

    Print Strings Followed By A New Line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point for the program.
+ *
+ * Perform initialization and call other functions to carry out the desired actions of the program.
+ *
+ * Returns:
+ *   0 on success, or a non-zero value on failure.
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
