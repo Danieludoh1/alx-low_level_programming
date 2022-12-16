9a42e63f62b888b11e9710d78d266e9d712e25a9
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-16 13:49:08 -0800

    Print Strings Followed By A New Line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point for the program.
  *
- * Perform initialization and call other functions to carry out the desired actions.
+ * Perform initialization and call other functions.
  * Returns:
  *   0 on success, or a non-zero value on failure.
  */
