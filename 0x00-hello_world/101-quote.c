3abf1a3c696f274b262a4ae74f39a11153399bd6
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-17 13:03:55 -0800

    Print Strings, Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 #include <unistd.h>
+
 /**
 * main - Entry Point
 * Return : Always 1
@@ -8,3 +10,4 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}
