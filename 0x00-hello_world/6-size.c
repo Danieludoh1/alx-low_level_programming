e375f973cd9870644ef5f1c09d6c4eb5b4292f05
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-17 11:07:00 -0800

    Print The Size Of Various Types On The Computer It Is Compiled And Run On

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
-int main(void)
-{
-	printf("Size of char: %ld bytes\n", sizeof(char));
-    	printf("Size of short: %ld bytes\n", sizeof(short));
-    	printf("Size of int: %ld bytes\n", sizeof(int));
-    	printf("Size of long: %ld bytes\n", sizeof(long));
-    	printf("Size of long long: %ld bytes\n", sizeof(long long));
-    	printf("Size of float: %ld bytes\n", sizeof(float));
-   	printf("Size of double: %ld bytes\n", sizeof(double));
-    	printf("Size of long double: %ld bytes\n", sizeof(long double));
 
-    	return 0;
+/**
+  * main - prints the size of various types on the computer
+  * it is compiled and run on
+  * Return: always 0
+  */
+
+int main (void)
+{
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+	printf ("Size of short: %ld byte(s)\n", sizeof(short));
+	printf("Size of int: %ld byte(s)\n", sizeof(int));
+	printf("size of long: %ld byte(s)\n", sizeof(long));
+	printf("size of long long: %ld byte(s)\n", sizeof(long long));
+	printf("Size of float: %ld byte(s)\n", sizeof(float));
+	printf("Size Of double: %ld byte(s)\n", sizeof(double));
+	printf("Size of long double: %ld byte(s)\n", sizeof(long double));
+	return (0);
 }
+
