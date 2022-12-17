d13f88a98da30c9c099c242371d05e6a68091eb8
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-17 11:19:59 -0800

    Print The Size Of Various Types On The Computer It Is Compiled And Run On

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,13 @@
   * Return: always 0
   */
 
-int main (void)
+int main(void)
 {
 	printf("Size of char: %ld byte(s)\n", sizeof(char));
-	printf ("Size of short: %ld byte(s)\n", sizeof(short));
 	printf("Size of int: %ld byte(s)\n", sizeof(int));
 	printf("size of long: %ld byte(s)\n", sizeof(long));
 	printf("size of long long: %ld byte(s)\n", sizeof(long long));
 	printf("Size of float: %ld byte(s)\n", sizeof(float));
-	printf("Size Of double: %ld byte(s)\n", sizeof(double));
-	printf("Size of long double: %ld byte(s)\n", sizeof(long double));
 	return (0);
 }
 
