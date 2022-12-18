fa869382b8090fda3da27aa5b1b29c7d0996f7db
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-18 14:22:58 -0800

    Print The Alphabets Followed By A New Line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - main block
+ * Description: Print the alphabets in lowercase
+ * Followed by a new line.
+ * Return: 0
+ */
 int main(void)
 {
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
+
 	{
 		putchar(c);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
