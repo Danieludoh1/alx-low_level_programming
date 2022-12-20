bd257f0daa99fd586f2f84b1445eb71a9af6e503
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-20 12:11:17 -0800

    Print The Alphabet In LowerCase, Except Q and E, Followed By A New Line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - main block
+*
+* Description: Print the alphabets in lowercase with The 
+* Exception Of "q & e" Followed by a new line.
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
