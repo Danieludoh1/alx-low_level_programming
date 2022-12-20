25795f9a960f6a5de691a86d35e9d24f2f2f92f9
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-20 12:12:24 -0800

    Print The Alphabet In LowerCase, Except Q and E, Followed By A New Line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 /**
 * main - main block
 *
-* Description: Print the alphabets in lowercase with The 
+* Description: Print the alphabets in lowercase with The
 * Exception Of "q & e" Followed by a new line.
 *
 * Return: 0
