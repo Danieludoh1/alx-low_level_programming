21c3414e3d769908bf37867d74b77ac3a08dc1c9
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-20 13:08:30 -0800

    Print All single Digits Of Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - main block
+
+* Description: Print all single digits of base 10
+* starting from 0, followed by a new line.
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for ( a = 0; a <= 9; a++)
+	{
+		printf("%d\n", a);
+		
+	}
+	return (0);
+}
