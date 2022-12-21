ec6141bd9201396cb6887ecfedcf4735e9240440
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-21 11:58:49 -0800

    Write All Possible Combo Of Single-Digits Numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - main block
+
+* Description: Print all The Possible Combination Of
+* Single-Digit Numbers
+* Followed by a new line.
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+
+	return (0);
+}
+
