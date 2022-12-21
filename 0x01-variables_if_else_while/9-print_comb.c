cc6484b7b57a625d81d4eb838662ed1175dc0f71
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-21 12:14:45 -0800

    Write All Possible Combo Of Single-Digits Numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 	}
 
+	putchar('9');
 	return (0);
 }
 
