cb8bf21870c7fb74fded73f05a3b48ac8c7c242b
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-24 13:22:35 -0800

    Task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*
+ * main - main block
+ *
+ * Description: Prints All Possible combinations of Two 
+ * Two-digit numbers.
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++) 
+	{
+		for (j = i; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			putchar(',');
+			putchar(' ');
+    }
+  }
+	putchar('\n');
+	return (0);
+}
