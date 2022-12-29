bc59bb98cea8f81543b097f4d597a92578f42c30
Danieludoh1 <Udoh17010111032@gmail.com>
2022-12-29 04:59:42 -0800

    Checks For Lowercase Char.

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -11,14 +11,14 @@ int main(void)
 	int r;
 
 	r = _islower('H');
-	putchar(r + '0');
+	_putchar(r + '0');
 	r = _islower('o');
-	putchar(r + '0');
+	_putchar(r + '0');
 	/*printf("%d", r);*/
 	r = _islower(108);
-	putchar(r+ '0');
+	_putchar(r+ '0');
 	/*printf("%d\n", r);*/
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
 
