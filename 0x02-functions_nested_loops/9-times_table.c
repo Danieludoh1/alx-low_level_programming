2361eb6a6a4f14a0fde704dacfd0d87b20917bfe
Danieludoh1 <Udoh17010111032@gmail.com>
2023-01-05 00:28:21 -0800

    Prints The 9 Times Table Starting With 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,30 +16,19 @@ void times_table(void)
 		{
 			int product = i * j;
 
-			/*printf("%d,", product);*/
-			/*if (product == 0)
-			{
-				printf("%d,  ", product);
-			}*/
-			/*if (product > 0 && product < 10)
-			{
-				printf("%2d, ", product);
-			}*/
 			if (j == 0)
 			{
 				printf("%d, ", product);
 			}
-			/*if (((product >= 1 || product < 10) && j <= 8) || (j != 0))*/
 			if ((j != 0) && j < 9)
 			{
 				printf("%2d, ", product);
 			}
 			if (j == 9)
-			{ 
+			{
 				printf("%2d", product);
 			}
 		}
 		printf("\n");
 	}
 }
-	
