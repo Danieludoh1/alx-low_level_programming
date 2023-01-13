3ef2f0341c55f843b6b08a4c459db0b9010070a4
Danieludoh1 <Udoh17010111032@gmail.com>
2023-01-13 21:19:23 +0100

    Find And Print The sum Of All Even FibonacciTerms Below 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,41 +1,21 @@
 #include <stdio.h>
-#include "main.h"
-
-/**
- *
- * Description: Prints The first 32 Even Numbers Fibonacci,
- * Followed by a new line.
- * Return: Always 0.
- */
 
 int main(void)
 {
-	/*long long i;*/
+    long long a = 0; // initialize a as the first Fibonacci number
+    long long b = 1; // initialize b as the second Fibonacci number
+    long long c = 0; // initialize c as the next Fibonacci number
+    long long sum = 0; // initialize sum to keep track of the even Fibonacci numbers
 
-	long long a = 2;
-
-	long long b = 4;
-
-	long long c, sum;
-
-
-c = 0;
-	while ((c < 4000000) && c % 2 == 0)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		sum += c;
-
-		c = c + 1;
-
-		/*if ( c % 2 == 0)
-		{
-			sum += c;
-		}*/
-	}
-	printf("%lld", sum);
-	return (0);
+    while (c <= 4000000) { // continue looping as long as c is less than 4,000,000
+        c = a + b; // calculate the next Fibonacci number
+        a = b; // update a to be the previous b
+        b = c; // update b to be the current c
+        if (c % 2 == 0) { // check if c is even
+            sum += c; // add c to the sum
+        }
+    }
+    printf("%lld\n", sum); // print the sum of the even Fibonacci numbers
+    return (0);
 }
-
 
