8017f8758c54d46a80e95a155268ba24cee5bb4c
Danieludoh1 <Udoh17010111032@gmail.com>
2023-01-22 21:05:20 +0100

    Prints The First 98 Fibonacci Numbers, Starting With 1 And 2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -14,15 +14,19 @@ int main(void)
 	long c = 0;
 	long sum = 0;
 
-	while (c <= 4000000)
-	{
+
+		while (c < 4000000)
+		{
 		c = a + b;
 		a = b;
 		b = c;
 		if (c % 2 == 0)
 		{
 			sum += c;
+		
+
 		}
+
 	}
 	printf("%ld\n", sum);
 	return (0);
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,11 +13,12 @@ int main(void)
 {
 	unsigned long int a = 1;
 
-	long double b = 2;
+	long int b = 2;
 
-	unsigned long int i, c, al, ar, bl, br, cl, cr, d; /*A, B, C, num, numm, nummc;*/
+	unsigned long int i, c, al, bl, cl, ar, d, br, cr; /*at, ab, bt, bb, ck ,ct; A, B, C, num, numm, ar, br nummc;*/
 
-		      printf("%lu, %lu, ", a, b);
+
+		      printf("%lu, %ld, ", a, b);
 
 	for (i = 3; i <= 92; i++)
 	{
@@ -27,43 +28,111 @@ int main(void)
 		printf("%lu, ", c);
 	}
 
-	a = 7540113804746346429;
-	b = 1.22002 * 10^17;
-	d = 100000000;
+
+	d = 1000000000000000;
 	al = a / d;
 	ar = a % d;
 	bl = b / d;
 	br = b % d;
 	cl = al + bl;
 	cr = ar + br;
-	printf("%lu %lu", cl, cr);
+	/*printf("\n%lu\n", ar);*/
+	/*printf("\n%lu %lu", ar br*/
+	/*printf("\n%lu%lu", cl, cr);*/
 
 
+	for (i = 0; i < 3; i++)
+	{
+		cl = al + bl;
+		cr = ar + br;
+		al = bl;
+		ar = br;
+		bl = cl;
+		br = cr;
+		printf("%lu%lu,  ", cl, cr);
+	}
 
 
-		/*for (i = 93; i < 99; i++)
+	for ( i = 0; i < 3; i++)
+		
+	{
+		cl = al + bl;
+		cr = ar + br;
+		al = bl;
+		ar = br;
+		bl = cl;
+		br = cr;
+		printf(i == 2 ? "%lu%lu" : "%lu%lu, ", (cl / 10), cr);
+
+	}
+		/*if ( i >= 5)
 		{
-
-			d = 100000000;
-			al = a / d;
-			ar = a % d;
-			bl = b / d;
-			br = b % d;
+			al = a / 1000000000000000;
+			ar = a % 100000000000000;
+			bl = b / 1000000000000000;
+			br = b % 100000000000000;
 			cl = al + bl;
-			cr = ar + br;
-			num = (int) log10(al) + 1;
-			A = (al << num) | ar;
-			numm = (int) log10(bl) + 1;
-			B = (bl << numm) | br;
-			nummc = (int) log10(cl) + 1;
-			C = (cl << nummc) | cr;
-			C = A + B;
-			A = B;
-			B = C;
+			cr = ar + br;*/
 
-			printf("%lu, ", C);
+			/*printf("\ndog%lu.%lu, ", cl, cr);*/
 
-		}*/
+
+	/*at = 8362114348984842297 / 1000000000000000;
+	ac = 8362114348984842297 % 1000000000000000;
+	printf("%lu..%lu", at, ac);*/
+	/*ab =  al / 10;*/
+
+	/*for (i = 0; i < 3; i++)*/
+	/*{
+
+
+			 d = 1000000000000;
+			 k = 1000000000000000;
+			 al = a / d;
+			 ar = a % k;
+			 bl = b / d;
+			 br = b % k;
+			 cl = al + bl;
+			 cr = ar + br;*/
+			 /*cl = ab + bl;
+			 cr = ar + br;
+			 ab = bl;
+			 ar = br;
+			 bl = cl;
+			 br = cr;
+			 printf("\n.%lu..", ab);
+			 printf("\n%lu%lu, ", cl, cr);
+	}*/
+
+
+
+
+			 /* al = a / 1000000000000;
+			bl = a / 1000000000000;
+			printf("\n%lu%lu, ", cl, cr);
+
+			printf("\n%lu . %lu", al, ar);*/
+
+	/*at = al;
+	ab = ar;
+	bt = bl;
+	bb = br;
+	ck = at + bt;
+	ct = ab + bb;
+
+	for ( i = 0; i <= 3; i++)
+	{
+		ck = at + bt;
+		ct = ab + bb;
+		at = bt;
+		ab = bb;
+		bt = ck;
+		bb = ct;
+		printf(" Donkey\n%lu...%lu, ", ck, ct);
+	}*/
+
+
+
 
 
 
