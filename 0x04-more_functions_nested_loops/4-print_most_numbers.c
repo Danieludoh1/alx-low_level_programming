f376832b318c9c289ba831ec945e15390cbb6567
Danieludoh1 <Udoh17010111032@gmail.com>
2023-02-08 22:07:09 +0100

    Print The Numbers, From 0 - 9, Excluding 2 & 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers from 0 - 9, 
+ * print_most_numbers - Prints the numbers from 0 - 9,
  * excluding 2 and 4
  * Return: void.
  */
