14bb27415141cd89ecbb6702daab9a8c8cc76126
Danieludoh1 <Udoh17010111032@gmail.com>
2023-02-08 11:53:40 +0100

    Multiply Two Numbers And Return The Result

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Multiplies Two Integers.
+ * int a: The Multiplier
+ * int b: The Multiplicand
+ * Return: The Result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return result;
+}
